6b98a14eceba0a52a8794385d1de9816fa50ebce
DESKTOP-MIV89QG\Taejun <tjkimin2021@gmail.com>
2022-05-03 17:45:45 +0900

    주문하기, 취소

--- shop/src/main/java/com/shop/entity/Order.java
+++ shop/src/main/java/com/shop/entity/Order.java
@@ -33,5 +33,37 @@ public class Order extends BaseEntity{
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY) // 부모 엔티티의 영속성 상태 변화를 자식 엔티티에 모두 전이한다.
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem); // 엔티티 안에 있는 orderItems에 더해주고
+        orderItem.setOrder(this); // order에 있는 리스트들을 새로 세팅해준다.
+    }
 
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member);
+        orderItemList.forEach(orderItem -> {
+            order.addOrderItem(orderItem);
+        });
+
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+    public void cancelOrder() {
+        this.orderStatus = OrderStatus.CANCEL;
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
 }
